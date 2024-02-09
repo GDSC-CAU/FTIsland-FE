990703c9241d214d7bb93edacbac874d5d291ab2
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2024-02-09 11:33:52 +0900

    feat: 언어 설정 창 구현 (#7)

--- src/components/side/LanguageSetting.tsx
+++ src/components/side/LanguageSetting.tsx
@@ -1,21 +1,47 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import Menu from './Menu';
+import { Box, SelectChangeEvent } from '@mui/material';
+import List from './List';
+import Back from './Back';
+import LanguageButton from './LanguageButton';
 
 interface MenuProps {
   setContent: (setContent: ReactElement) => void;
   onClick: (content: string) => void;
   handleSideMenu: (content: boolean) => void;
 }
+
 const LanguageSetting: React.FC<MenuProps> = ({setContent, onClick, handleSideMenu}) => {
-    const handleBack = () => {
-        setContent(<Menu setContent={setContent} onClick={onClick} handleSideMenu={handleSideMenu} />);
-      };
+  
+  const [mainLanguage, setMainLanguage] = useState('한국어');
+  const [subLanguage, setSubLanguage] = useState('English');
+  
+  const handleBack = () => {
+    setContent(<Menu setContent={setContent} onClick={onClick} handleSideMenu={handleSideMenu} />);
+  };
+
+  const handleMainLanguageChange = (event: SelectChangeEvent<string>) => {
+    setMainLanguage(event.target.value as string);
+    console.log(event.target.value as string);
+  };
+  const handleSubLanguageChange = (event: SelectChangeEvent<string>) => {
+    setSubLanguage(event.target.value as string);  // 클릭된 내용을 mainLanguage 상태에 저장
+  };
+
   return (
-    <div>
-        <button onClick={handleBack}></button>
-        <button></button>
-    </div>
+    <Box sx={{bgcolor: "#FFE5E5", height: "100%"}}>
+
+      <Back handleBack={handleBack}/>
+      
+      <List content={"주언어"}/>
+      <LanguageButton language={mainLanguage} handleLanguageChange={handleMainLanguageChange}/>
+      
+
+      <List content={"서브 언어"}/>
+      <LanguageButton language={subLanguage} handleLanguageChange={handleSubLanguageChange}/>
+      
+    </Box>
   )
 }
 
-export default LanguageSetting
+export default LanguageSetting;
