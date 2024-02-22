f728478f48eb1ae263bdd7e5c9c5864d020a2d93
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2024-02-22 13:59:51 +0900

    feat: 부모로 전환 버튼 클릭하면 userId 바뀌는 기능 추가 (#41)

--- src/components/side/components/ParentButton.tsx
+++ src/components/side/components/ParentButton.tsx
@@ -1,29 +1,35 @@
 import { Avatar, Box, Typography } from '@mui/material'
 import React, { useState } from 'react'
+import { useUser } from 'src/hook/useUser';
 
 const ParentButton = () => {
+  const {userId, setUserId} = useUser();
   const [isParent, setIsParent] = useState(false);
 
   const handleParent = () => {
+    const difference = isParent ? -1 : 1;
+    setUserId(userId + difference);
     setIsParent(!isParent);
   }
 
   return (
     <Box sx={{bgcolor: "#39A7FF", padding: 1.2, boxShadow: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '7%'}}>
     {
-
+      userId > 0 &&
+      <>
+      <Typography variant="h4" sx={listStyle()} >부모로 전환</Typography>
+      <Avatar
+      src={
+        isParent
+          ? '/image/on.webp'
+          : '/image/off.webp'
+      }
+      alt="arrow right"
+      sx={{ width: '95px', height: '30px', borderRadius: '0px', bgColor: 'none' }}
+      onClick={handleParent}
+      />
+      </>
     }
-    <Typography variant="h4" sx={listStyle()} >부모로 전환</Typography>
-    <Avatar
-    src={
-      isParent
-        ? '/image/on.webp'
-        : '/image/off.webp'
-    }
-    alt="arrow right"
-    sx={{ width: '95px', height: '30px', borderRadius: '0px', bgColor: 'none' }}
-    onClick={handleParent}
-  />
   </Box>
   )
 }
